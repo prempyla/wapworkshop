a4d7f5b976122ee3106e8692a77935304a2b1889
prempyla <pylaprem@gmail.com>
2025-04-12 15:31:59 +0530

    pb connection

--- wapworkshop/src/App.jsx
+++ wapworkshop/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-
-
+import { useState ,useEffect} from 'react'
+import PocketBaseConnectionCreator from 'pocketbase'
+const pb=new PocketBaseConnectionCreator('http://127.0.0.1:8090')
 function App() {
 
+const [categories,setcategories]=useState([])
+ useEffect(()=>{
+  async function fetchData(){
+    const records = await pb.collection('categories').getFullList()
 
-  return (
-   <h1>hello</h1>
-  )
+    setcategories(records)
+  }
+  fetchData()
+ },[])
+ return (
+  <>
+    <h1>Categories</h1>
+    <ul>
+      {categories.map((cat) => (
+        <li key={cat.id}>{cat.name}</li>
+      ))}
+    </ul>
+  </>
+)
+
 }
 
 export default App
