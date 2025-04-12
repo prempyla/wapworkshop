51b3cc0f86f03485ed647be5b62f61be80b69e91
prempyla <pylaprem@gmail.com>
2025-04-12 16:05:43 +0530

    pb data fetch

--- wapworkshop/src/App.jsx
+++ wapworkshop/src/App.jsx
@@ -1,28 +1,47 @@
-import { useState ,useEffect} from 'react'
-import PocketBaseConnectionCreator from 'pocketbase'
-const pb=new PocketBaseConnectionCreator('http://127.0.0.1:8090')
+import { useState, useEffect } from 'react';
+import './App.css';
+import PocketBaseConnectionCreator from 'pocketbase';
+const pb = new PocketBaseConnectionCreator('http://127.0.0.1:8090');
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const records = await pb.collection('Categories').getFullList();
+        console.log(records);
+        setData(records);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    }
+    getData()
+  }, []);
+  return (
+    <>
+      <h1>Expenss Manager</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Item Description</th>
+            <th>Color</th>
+            <th>Cost</th>
 
-const [categories,setcategories]=useState([])
- useEffect(()=>{
-  async function fetchData(){
-    const records = await pb.collection('categories').getFullList()
-
-    setcategories(records)
-  }
-  fetchData()
- },[])
- return (
-  <>
-    <h1>Categories</h1>
-    <ul>
-      {categories.map((cat) => (
-        <li key={cat.id}>{cat.name}</li>
-      ))}
-    </ul>
-  </>
-)
-
+  
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item,index) => (
+            <tr key={item.id}>
+              <td>{index+1}</td>
+              <td>{item.name}</td>
+              <td>{item.color}</td>
+              <td>{item.Cost}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
-
-export default App
+export default App;
